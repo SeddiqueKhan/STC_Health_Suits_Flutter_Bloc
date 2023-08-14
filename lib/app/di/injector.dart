2002f41aaded5ce8ccc601066e154c480274732c
Seddique Khan <seddique.ahmed@farmtohome.com.pk>
2023-08-14 20:43:46 +0500

    Implemented MVVM Architecture.

---
+++ lib/app/di/injector.dart
@@ -0,0 +1,8 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import '../../common/constants.dart';
+
+final injector = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+}
