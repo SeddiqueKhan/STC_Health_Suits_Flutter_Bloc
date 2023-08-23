101b7d606f8ce7ad183e679ef05d2acaa22d21bc
Seddique Khan <seddique.ahmed@farmtohome.com.pk>
2023-08-23 17:38:48 +0500

    Products Count error fixed.

--- lib/presentation/Home/HomeScreen.dart
+++ lib/presentation/Home/HomeScreen.dart
@@ -112,7 +112,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
-          SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+          SliverList(delegate: SliverChildBuilderDelegate(childCount: getAllProductsModel.products?.length,(context, index) {
             var product = getAllProductsModel.products?[index];
             return _card(product);
           }))
